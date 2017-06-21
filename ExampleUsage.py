92eefcf84fd4f25fb36ac722cc212137a155c996
Ash Setter <ajs3g11@soton.ac.uk>
2017-06-21 15:31:18 +0100

    updated example

--- ExampleUsage.py
+++ ExampleUsage.py
@@ -1,12 +1,12 @@
 import numpy as np
 import qplots
 
-import seaborn # this breaks my fontsize argument =/
+#import seaborn # this breaks my fontsize argument =/
 #seaborn.set_style("whitegrid") 
 
 # the random data
 x = np.random.randn(100000)
-y = np.random.randn(100000)
+y = np.random.randn(100000) 
 
 fig, axscatter, axhistx, axhisty, cm = qplots.joint_plot(x, y, fontsize=8)
 axscatter.set_xlabel("variable 1")
